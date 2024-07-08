1cba98da6dfb3dbf6da834e57db4aede1d56a37b
Jason <jdnew18@gmail.com>
2024-07-08 11:10:34 -0400

    Updated namespaces

--- CodingPractice/ClockAngle.cs
+++ CodingPractice/ClockAngle.cs
@@ -1,30 +1,30 @@
-namespace CodingPractice;
+namespace CodingPractice {
+    public static class ClockAngle
+    {
+        // Given 12-hour clock time in intervals as small as 5 minutes,
+        // determine angle between hour hand and minute hand.
+        public static int Calculate(int hour, int minute) {
+            // Validate input.
+            if (hour < 1 || hour > 12 || minute < 0 || minute > 59) {
+                throw new Exception("Invalid input: Hours must be between 1 and 12 and minutes must be between 0 and 59.");
+            }
 
-public static class ClockAngle
-{
-    // Given 12-hour clock time in intervals as small as 5 minutes,
-    // determine angle between hour hand and minute hand.
-    public int Calculate(int hour, int minute) {
-        // Validate input.
-        if (hour < 1 || hour > 12 || minute < 0 || minute > 59) {
-            throw new Exception("Invalid input: Hours must be between 1 and 12 and minutes must be between 0 and 59.");
+            // Get positive angle of hour hand.
+            int hourAngle = hour * 30;
+
+            // Get positive angle of minute hand.
+            int minuteAngle = minute * 6;
+
+            // Get absolute value of difference between angles.
+            int difference = Math.Abs(hourAngle - minuteAngle);
+
+            // If difference greater than 180, calculate difference from 360.
+            if (difference > 180) {
+                difference = 360 - difference;
+            }
+
+            // Return result.
+            return difference;
         }
-
-        // Get positive angle of hour hand.
-        int hourAngle = hour * 30;
-
-        // Get positive angle of minute hand.
-        int minuteAngle = minute * 6;
-
-        // Get absolute value of difference between angles.
-        int difference = Math.Abs(hourAngle - minuteAngle);
-
-        // If difference greater than 180, calculate difference from 360.
-        if (difference > 180) {
-            difference = 360 - difference;
-        }
-
-        // Return result.
-        return difference;
     }
 }
--- CodingPractice/ClosestToZero.cs
+++ CodingPractice/ClosestToZero.cs
@@ -1,42 +1,42 @@
-namespace CodingPractice;
-
-public static class ClosestToZero
-{
-    // Return number in array whose absolute value is closest to zero.
-    public double Find(double[] ts) {
-        // If empty, return 0.
-        if (ts.Length == 0) {
-            return 0;
-        }
-
-        double closest = ts.First();
-        double absClosest = GetAbsoluteValue(closest);
-
-        // Iterate through array to find number closest to 0.
-        foreach (double currTemp in ts) {
-            double absCurrTemp = GetAbsoluteValue(currTemp);
-
-            // Compare, ensure positive number wins.
-            if (absCurrTemp <= absClosest) {
-                // Positive current temperature wins.
-                if (currTemp >= 0) {
-                    closest = currTemp;
-                }
-                // Negative current temperature only wins
-                // if its absolute value is less than closest's absolute value.
-                else if (absCurrTemp != absClosest) {
-                    closest = currTemp;
-                }
-                
-ß               // Update.
-                absClosest = GetAbsoluteValue(closest);
+namespace CodingPractice {
+    public static class ClosestToZero
+    {
+        // Return number in array whose absolute value is closest to zero.
+        public static double Find(double[] ts) {
+            // If empty, return 0.
+            if (ts == null || ts.Length == 0) {
+                return 0;
             }
+
+            double closest = ts.First();
+            double absClosest = System.Math.Abs(closest);
+
+            // Iterate through array to find number closest to 0.
+            foreach (double currTemp in ts) {
+                double absCurrTemp = System.Math.Abs(currTemp);
+
+                // Compare, ensure positive number wins.
+                if (absCurrTemp <= absClosest) {
+                    // Positive current temperature wins.
+                    if (currTemp >= 0) {
+                        closest = currTemp;
+                    }
+                    // Negative current temperature only wins
+                    // if its absolute value is less than closest's absolute value.
+                    else if (absCurrTemp != absClosest) {
+                        closest = currTemp;
+                    }
+                    
+                   // Update.
+                    absClosest = System.Math.Abs(closest);
+                }
+            }
+
+            return closest;
         }
 
-        return closest;
-    }
-
-    double GetAbsoluteValue(double temp) {
-        return temp >= 0 ? temp : temp * (-1);
+        static double GetAbsoluteValue(double temp) {
+            return temp >= 0 ? temp : temp * (-1);
+        }
     }
 }
