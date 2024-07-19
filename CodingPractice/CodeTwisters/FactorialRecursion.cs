8a4882f95dea5ef3bc3564d17b9799d677816b0f
Jason <jdnew18@gmail.com>
2024-07-19 16:09:46 -0400

    Calculate factorial recursively.

---
+++ CodingPractice/CodeTwisters/FactorialRecursion.cs
@@ -0,0 +1,18 @@
+namespace CodingPractice.CodeTwisters {
+    // Calculate the factorial (n!) of a given integer using recursion.
+    public static class FactorialRecursion {
+        public static int Calculate(int factorial) {
+            // Throw an exception if factorial requested in less than 0.
+            if (factorial < 0) {
+                throw new Exception("Factorial must be 0 or greater.");
+            }
+
+            return Calculate(factorial, 1);
+        }
+
+        private static int Calculate(int factorial, int product) {
+            product *= factorial;
+            return --factorial > 0 ? Calculate(factorial, product) : product; 
+        }
+    }
+}
