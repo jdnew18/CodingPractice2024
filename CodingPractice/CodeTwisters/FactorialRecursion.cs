c3f0676dd68c26ed5893d72e5203eabb3eda78ac
Jason <jdnew18@gmail.com>
2024-07-20 21:33:57 -0400

    Revert "Fixed  bug in FactorialRecursion where 0! did not equal 1. Optimized and refactored."
    This reverts commit 9b28dfbe34dc13ccfcd158383d33959f7a71836d.

--- CodingPractice/CodeTwisters/FactorialRecursion.cs
+++ CodingPractice/CodeTwisters/FactorialRecursion.cs
@@ -1,21 +1,20 @@
 namespace CodingPractice.CodeTwisters {
     // Calculate the factorial (n!) of a given integer using recursion.
     public static class FactorialRecursion {
-        public static int Do(int factorial) {
+        public static int Calculate(int factorial) {
             // Throw an exception if factorial requested in less than 0.
             if (factorial < 0) {
                 throw new Exception("Factorial must be 0 or greater.");
             }
 
-            // 0! is 1, otherwise calculate.
-            return factorial > 0 ? Calculate(factorial) : 1;
+            return Calculate(factorial, 1);
         }
 
-        // Calculate product of the factorial,
-        // subtracting 1 from the requested factorial
+        // Calculate product of the factorial, subtracting 1 from the requested factorial
         // until the requested factorial is 0.
-        private static int Calculate(int factorial) {
-            return --factorial > 0 ? factorial * Calculate(factorial - 1) : product; 
+        private static int Calculate(int factorial, int product) {
+            product *= factorial;
+            return --factorial > 0 ? Calculate(factorial, product) : product; 
         }
     }
 }
